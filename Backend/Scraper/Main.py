867e903eadb4dd4e99c4eabd9267b6edd797da48
Himansh Gupta <53445286+HimanshGupta10@users.noreply.github.com>
2020-05-23 21:37:47 +0530

    Update Main.py

--- Backend/Scraper/Main.py
+++ Backend/Scraper/Main.py
@@ -1,11 +1,13 @@
 import IndiaToday
 import IndiaTV
 import IndianExpress
+import covid
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017")
 db = client['NewsCluster']
 db.news.drop()
+db.covid.drop()
 
 IndiaToday.scrape()
 IndianExpress.scrape()
